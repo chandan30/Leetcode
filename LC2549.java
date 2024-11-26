ec213cc8dd1c71d032d194544f682822e31e5cef
chandan30 <chandan98sharma111@gmail.com>
2024-11-26 20:28:02 +0530

    2549. Count Distinct Numbers on Board

---
+++ LC2549.java
@@ -0,0 +1,29 @@
+/*
+ * You are given a positive integer n, that is initially placed on a board. Every day, for 109 days, you perform the following procedure:
+
+For each number x present on the board, find all numbers 1 <= i <= n such that x % i == 1.
+Then, place those numbers on the board.
+Return the number of distinct integers present on the board after 109 days have elapsed.
+
+Note:
+
+Once a number is placed on the board, it will remain on it until the end.
+% stands for the modulo operation. For example, 14 % 3 is 2.
+ * */
+import java.util.Scanner;
+
+public class LC2549 {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int result = distinctIntegers(n);
+		System.out.println(result);
+		sc.close();
+	}
+	public static  int distinctIntegers(int n) {
+	    if(n==1)
+	    return 1;
+	    else
+	    return n-1;
+	}
+}
